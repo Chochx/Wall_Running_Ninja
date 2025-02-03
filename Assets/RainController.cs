d623d12d396320dbf1ab4321140709b7e010417c
Chochx <143450790+Chochx@users.noreply.github.com>
2025-02-03 12:21:49 +0100

    cc

---
+++ Assets/RainController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainController : MonoBehaviour
+{
+    private ParticleSystem particleSystem;
+
+    private void Start()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    private void SetRainVelocity()
+    {
+        
+    }
+}
